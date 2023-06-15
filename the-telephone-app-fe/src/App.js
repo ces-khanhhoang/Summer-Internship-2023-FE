87ea4f05419a93128106b56acec6a226859a6900
Thoa <thoathoa0110@gmail.com>
2023-06-15 14:26:48 +0700

    UI join game

--- the-telephone-app-fe/src/App.js
+++ the-telephone-app-fe/src/App.js
@@ -3,12 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WriteASentence from "./components/WriteASentence";
 import DrawAPicture from "./components/DrawAPicture";
+import JoinRoom from './components/JoinRoom/JoinRoom';
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path={"/"} element={<StartGame />}></Route>
+        <Route path={"/join"} element={<JoinRoom />}></Route>
+
         <Route path={"/sentence"} element={<WriteASentence />}></Route>
         <Route path={"/draw"} element={<DrawAPicture
           width={700}
