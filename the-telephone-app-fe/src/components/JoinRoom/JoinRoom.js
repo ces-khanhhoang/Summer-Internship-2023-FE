7e575918b8ff6b19d1c5c30f706599a1ff66ccfd
Thoa <thoathoa0110@gmail.com>
2023-06-19 08:45:02 +0700

    11

--- the-telephone-app-fe/src/components/JoinRoom/JoinRoom.js
+++ the-telephone-app-fe/src/components/JoinRoom/JoinRoom.js
@@ -1,4 +1,4 @@
-import './JoinRoom.css'
+import './JoinRoom.css';
 import { BsFillCaretLeftFill } from "react-icons/bs";
 import { BsFillVolumeUpFill } from "react-icons/bs";
 import { BsFillSendXFill } from "react-icons/bs";
@@ -9,56 +9,56 @@ import imgLogo from '../../assets/gartic-phone.svg';
 import imgNormal from '../../assets/normal.svg';
 const JoinRoom = () => {
     return (  
-        <div className="screen">
-            <div className="content">
-                <div className="header">
-                    <button className="btn-back">
+        <div className="jr-screen">
+            <div className="jr-content">
+                <div className="jr-header">
+                    <button className="jr-btn-back">
                         <BsFillCaretLeftFill />
                         BACK
                     </button>
-                    <img src={imgLogo} alt="logo"  className='img-logo'/>
-                    <button className="btn-sound">
+                    <img src={imgLogo} alt="logo"  className='jr-img-logo'/>
+                    <button className="jr-btn-sound">
                         <BsFillVolumeUpFill size={'24px'} />
                     </button>
                 </div>
-                <div className="center">
-                    <div className="left">
-                        <h4 className="left-player">Player</h4>
-                        <div className="user">
-                            <span className='choice-number'>
+                <div className="jr-center">
+                    <div className="jr-left">
+                        <h4 className="jr-left-player">Player</h4>
+                        <div className="jr-user">
+                            <span className='jr-choice-number'>
                                 <select name="" id="">
                                     <option value="">1</option>
                                 </select>
                             </span>
-                            <div className="player">
-                                <div className="detail-player">
-                                    <img src={imgAvatar} alt="avatar" className='img-avatar' />
+                            <div className="jr-player">
+                                <div className="jr-detail-player">
+                                    <img src={imgAvatar} alt="avatar" className='jr-img-avatar' />
                                     <span>CoolNickName</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="right">
-                        <div className="setting">
-                            <div className="tab">
-                                <h4 className="column">Preset</h4>
-                                <h4 className="column">setting</h4>
+                    <div className="jr-right">
+                        <div className="jr-setting">
+                            <div className="jr-tab">
+                                <h4 className="jr-column">Preset</h4>
+                                <h4 className="jr-column">setting</h4>
                             </div>
-                            <div className="data">
-                                <div className="detail">
-                                    <div className="detail-setting">
-                                        <img src={imgNormal} alt="img-setting" className='img-setting' />
+                            <div className="jr-data">
+                                <div className="jr-detail">
+                                    <div className="jr-detail-setting">
+                                        <img src={imgNormal} alt="img-setting" className='jr-img-setting' />
                                         <span>CoolNickName</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="action">
-                            <button className="btn-action">
+                        <div className="jr-action">
+                            <button className="jr-btn-action">
                                 <BsFillSendXFill />
                                 Invite
                             </button>
-                            <button className="btn-action">
+                            <button className="jr-btn-action">
                                 <BsFillArrowRightSquareFill />
                                 Start 
                             </button>
