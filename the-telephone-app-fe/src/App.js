144fd14a1ca0dde35ff938fdaf7e8f6dfdd40d3e
Thoa <thoathoa0110@gmail.com>
2023-07-04 14:52:23 +0700

    handle button play again

--- the-telephone-app-fe/src/App.js
+++ the-telephone-app-fe/src/App.js
@@ -1,12 +1,10 @@
 import StartGame from "./components/StartGame/StartGame";
 import { Route, Routes } from "react-router-dom";
-import JoinRoom from "./components/JoinRoom/JoinRoom";
-import ShowResult from "./components/ShowResult/ShowResult";
+import JoinRoom from './components/JoinRoom/JoinRoom';
+import ShowResult from './components/ShowResult/ShowResult'
 import DescribePicture from "./components/DescribePicture/DescribePicture";
 import Draw from "./components/Draw/Draw";
-import WriteSentence from "./components/WriteSentence/WriteSentence";
-import WebFont from "webfontloader";
-import { useEffect } from "react";
+import WriteSentence from './components/WriteSentence/WriteSentence';
 function App() {
   useEffect(() => {
     WebFont.load({
@@ -16,16 +14,17 @@ function App() {
     });
   }, []);
   return (
-    <>
-      <Routes>
-        <Route path={"/lobby"} element={<JoinRoom />}></Route>
-        <Route path={"/:id_room?"} element={<StartGame />}></Route>
-        <Route path={"/start"} element={<WriteSentence />}></Route>
-        <Route path={"/draw"} element={<Draw />}></Route>
-        <Route path={"/write"} element={<DescribePicture />}></Route>
-        <Route path={"/book"} element={<ShowResult />}></Route>
-      </Routes>
-    </>
+      <>
+        <Routes>
+          <Route path={"/lobby"} element={<JoinRoom />}></Route>
+          <Route path={"/:id_room?"} element={<StartGame />}></Route>
+          <Route path={"/start"} element={<WriteSentence />}></Route>
+          <Route path={"/draw"} element={<Draw />}></Route>
+          <Route path={"/write"} element={<DescribePicture />}></Route>
+          <Route path={"/book"} element={<ShowResult />}></Route>
+        </Routes>
+      </>   
+    
   );
 }
 
